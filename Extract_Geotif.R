3bd30c3553e4547500ca5c7ad04afbf7adc2e85e
EMRAgit <49976179+EMRAgit@users.noreply.github.com>
2019-10-28 11:34:36 +0100

    Update Extract_Geotif.R

--- Extract_Geotif.R
+++ Extract_Geotif.R
@@ -1,19 +1,17 @@
 # Used to create the velox objects before launching the application.
 # W.DIR: path to the folder of the PhenoWin app
 # GEOTIF.DIR: path to the folder containing all the phenological raster
-
-W.DIR <- dirname(rstudioapi::getActiveDocumentContext()$path)
-GEOTIF.DIR = "..."
+W.DIR = dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(W.DIR)
 
+source("LoadAndInstallPackages.R")
 source("Functions_Pheno.R")
 # Functions_Pheno.R contain the DATA_FILE function.
 # This function is used to create the path in which
 # the velox object is stored
 
-library(tidyverse)
-library(lubridate)
-
+LoadAndInstallPackages()
+GEOTIF.DIR = paste(W.DIR,"/_Data/",sep="")
 
 tif_info = extract_tif_info(GEOTIF.DIR)
 crop_list = unique(tif_info$Crop)
